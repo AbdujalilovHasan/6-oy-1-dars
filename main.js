18d3572cf7f0f007490cfe71ce7777b7a7744272
AbdujalilovHasan <hasanabdujalilov171@gmail.com>
2024-07-10 15:44:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    let timeString = hours + ':' + minutes + ':' + seconds;
+
+
+    document.getElementById('clock').textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
